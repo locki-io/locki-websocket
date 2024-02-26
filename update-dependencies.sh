5fb5840cdf5bbb1cc045f3915565623c91297ea0
satishnvrn <nvrn.satish@gmail.com>
2024-02-26 23:51:14 +0530

    initial commit

---
+++ update-dependencies.sh
@@ -0,0 +1,3 @@
+cd onconnect && npx npm-check-updates -u && npm install && cd ..
+cd ondisconnect && npx npm-check-updates -u && npm install && cd ..
+cd sendmessage && npx npm-check-updates -u && npm install && cd ..
